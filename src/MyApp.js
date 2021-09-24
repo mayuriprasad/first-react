8367fcdb2a38b446ab9d2a356deef334fbe2d6f8
mayuriprasad <maprasad@calpoly.edu>
2021-09-24 13:43:39 -0700

    added delete feature

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Table from './Table'
+import React, {useState} from 'react';
+import Table from './Table';
 
-const characters = [
+/* const characters = [
     {
       name: 'Charlie',
       job: 'Janitor',
@@ -18,14 +18,38 @@ const characters = [
       name: 'Dennis',
       job: 'Bartender',
     },
-];
+]; */
 
 function MyApp() { 
-    return ( 
-        <div className="container">
-        <Table characterData={characters} />
-      </div>
-    );  
-}   
+  const [characters, setCharacters] = useState([
+    {
+      name: 'Charlie',
+      job: 'Janitor',
+    },
+    {
+      name: 'Mac',
+      job: 'Bouncer',
+    },
+    {
+      name: 'Dee',
+      job: 'Aspring actress',
+    },
+    {
+      name: 'Dennis',
+      job: 'Bartender',
+    },
+  ]);
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+        return i !== index
+      });
+      setCharacters(updated);
+  }
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    </div>
+  )
+}
 
 export default MyApp;
--- src/Table.js
+++ src/Table.js
@@ -14,27 +14,30 @@ function TableHeader()  {
   function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
         return (
-          <tr key={index}>
-            <td>{row.name}</td>         
-            <td>{row.job}</td>
-          </tr>
+        <tr key={index}>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
+          <td>
+              <button onClick={() => props.removeCharacter(index)}>Delete</button>
+          </td>
+        </tr>
         );
        }
       );
       return (
-          <tbody>
-            {rows}
-           </tbody>
-       );
+        <tbody>
+          {rows}
+        </tbody>
+      );
   }
 
-function Table(props) {
+  function Table (props) {
     return (
       <table>
         <TableHeader />
-        <TableBody characterData={props.characterData}/>
+        <TableBody characterData={props.characterData} removeCharacter={props.removeCharacter} />
       </table>
     );
-}
+  }
 
 export default Table;
